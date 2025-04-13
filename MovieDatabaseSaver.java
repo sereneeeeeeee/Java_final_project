f57028613651f9abd618af9973e5c5007c9d760d
Karen-0123 <151003065+Karen-0123@users.noreply.github.com>
2025-04-13 15:33:27 +0800

    存進資料庫

---
+++ MovieDatabaseSaver.java
@@ -0,0 +1,56 @@
+public class MovieDatabaseSaver {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/your_database";
+    private static final String USER = "your_user";
+    private static final String PASSWORD = "your_password";
+
+    public static void saveMovies(List<Movie> movies) {
+        String insertMovieSQL = "INSERT INTO movies (title, image, url, link, director, overview, year) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
+
+            for (Movie movie : movies) {
+                // 插入主表
+                PreparedStatement pstmt = conn.prepareStatement(insertMovieSQL, Statement.RETURN_GENERATED_KEYS);
+                pstmt.setString(1, movie.getTitle());
+                pstmt.setString(2, movie.getImage());
+                pstmt.setString(3, movie.getUrl());
+                pstmt.setString(4, movie.getLink());
+                pstmt.setString(5, movie.getDirector());
+                pstmt.setString(6, movie.getOverview());
+                pstmt.setInt(7, movie.getYear());
+                pstmt.executeUpdate();
+
+                // 取得自動產生的 movie_id
+                ResultSet rs = pstmt.getGeneratedKeys();
+                int movieId = 0;
+                if (rs.next()) {
+                    movieId = rs.getInt(1);
+                }
+
+                // 插入演員
+                String insertActorSQL = "INSERT INTO movie_actors (movie_id, actor_name) VALUES (?, ?)";
+                for (String actor : movie.getActors()) {
+                    PreparedStatement actorStmt = conn.prepareStatement(insertActorSQL);
+                    actorStmt.setInt(1, movieId);
+                    actorStmt.setString(2, actor);
+                    actorStmt.executeUpdate();
+                }
+
+                // 插入分類
+                String insertGenreSQL = "INSERT INTO movie_genres (movie_id, genre_name) VALUES (?, ?)";
+                for (String genre : movie.getGenre()) {
+                    PreparedStatement genreStmt = conn.prepareStatement(insertGenreSQL);
+                    genreStmt.setInt(1, movieId);
+                    genreStmt.setString(2, genre);
+                    genreStmt.executeUpdate();
+                }
+            }
+
+            System.out.println("✅ 所有電影成功儲存到資料庫！");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
