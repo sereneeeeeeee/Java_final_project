1fc9718a3f1b5b0cd056783a0fab623e37cab23c
Karen-0123 <151003065+Karen-0123@users.noreply.github.com>
2025-04-13 14:09:27 +0800

    add movie class

---
+++ movie.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private String image;
+    private String url;
+    private String link;
+    private String director;
+    private List<String> actors;
+    private String overview;
+    private int year;
+    private List<String> genre;
+
+    // Getters and Setters
+}
