49cab2584c6d0fe29e350e0ac4407e4721a9caea
Karen-0123 <151003065+Karen-0123@users.noreply.github.com>
2025-04-21 19:12:35 +0800

    .

--- MovieParser.java
+++ MovieParser.java
@@ -1,6 +1,5 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.FileReader;
 import java.util.List;
 
@@ -9,7 +8,6 @@ public class MovieParser {
     public static List<Movie> parseJson(String filePath) throws Exception {
         Gson gson = new Gson();// 把 JSON 自動轉成 Java 物件
         FileReader reader = new FileReader(filePath);// 打開指定路徑的 JSON 檔案來讀取內容。
-
         return gson.fromJson(reader, new TypeToken<List<Movie>>() {
         }.getType());// 把 JSON 檔案內容 ➜ 轉成 Java 的 List<Movie>，傳回 List<Movie>。
     }
